576d905798e6605eb830a5cdbf0ac631297bba89
edmondfrancoismillionnaires <51387125+edmondfrancoismillionnaires@users.noreply.github.com>
2020-01-08 10:51:43 +0100

    Update test.py

--- package/test.py
+++ package/test.py
@@ -3,12 +3,12 @@ def ispair(n):
         return 'pair'
     return 'impair'
 
-def typic_branch(n):
+def hamza_graph(n):
     if n % 2 == 0:
         n = 2*n + 1
-    else n % 2 == 1:
+    else:
         n = 2*n
     if n % 2 == 0:
         return 0
-    else n % 2 == 1:
+    else:
         return 1
